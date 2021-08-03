601fd19b4d95efc2c9bd19a72de28fea13c192cc
Gagandeep-Kaur-01 <gagandeep07kaur@gmail.com>
2021-08-03 15:29:50 +0530

    converted to class component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,27 @@
 import React, { Component } from 'react'
+import Calculator from '../src/components/Calculator'
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+       <span className="App-header">
+         Calculator
+       </span>
+       <Calculator />
+      </div>
+    )
+  }
+}
+
+export default App
+
+
+
+/*  Functional component
+
+import React, { Component } from 'react'
 import './App.css';
 import Calculator from '../src/components/Calculator'
 
@@ -14,3 +37,5 @@ function App() {
 }
 
 export default App;
+
+*/
