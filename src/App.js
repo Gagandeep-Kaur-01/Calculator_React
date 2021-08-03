52163c8f49f2afee101457204daa2444c8b16cad
Gagandeep-Kaur-01 <gagandeep07kaur@gmail.com>
2021-08-03 12:03:56 +0530

    deleted unusual code and added little css

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      Calculator
+      <span className="App-header">
+        Calculator
+      </span>
     </div>
   );
 }
